4c0881470033d4d4eadf9b3be5a9e2be414e9e5f
sunchutian <czs0092@tigermail.auburn.edu>
2019-08-29 00:10:47 -0500

    Chutian Sun Auburn

--- src/main/java/FinderTest.java
+++ src/main/java/FinderTest.java
@@ -1,19 +1,20 @@
-//new
+import org.junit.*;
+import static org.junit.Assert.assrtEquals;
 
 public class FinderTest {
     @Before public void setUp() {
 
     }
 
-    @Test public void findMaxTest{
-        Finder.findMax max = new Finder.findMax();
+    @Test public void findMaxTest(){
+        Finder max = new Finder();
        int[] array = {1,3,5,7,8,10};
-       assertEquals(10, Finder.finderMax(array));
+       assertEquals(expected: 10, Finder.finderMax(array));
     }
 
-    @Test public void findMinTest{
+    @Test public void findMinTest(){
         Finder.findMin max = new Finder.findMin();
         int[] array = {10,3,5,7,2,14};
-        assertEquals(2, Finder.finderMin(array));
+        assertEquals(expected: 2, Finder.finderMin(array));
     }
 }
